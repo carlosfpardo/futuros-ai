c7008483fe9b7aafa6369cde16d365540b91cae4
Carlos Efe Pardo <48070328+carlosfpardo@users.noreply.github.com>
2023-11-05 09:00:38 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ cexport const getGlobalData = () => {
     : 'Futuros AI';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Un taller para aplicar IA a su empresa de manera efectiva, ética y responsable';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Derechos reservados.';
